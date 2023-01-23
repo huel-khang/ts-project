50f4e2ae73793699e4f5938851bfcdf418a9c67f
khang <khang@huel.com>
2023-01-23 13:55:08 +0000

    tsconfig learning complete

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
-console.log("Put code in here...")
+let appID = 'abc';
 const button = document.querySelector('button')!;
+
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  return;
+}
 
 function clickHandler(message: string) {
   console.log('Clicked! ' + message)
 }
 
-button?.addEventListener('click', () => {
-  console.log('Clicked!');
-});
+button?.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
